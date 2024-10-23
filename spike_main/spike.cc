bcfa74d11607d02df6b71ae0b499bc9191ae20f3
mslijepc <mladen.slijepcevic@sifive.com>
2024-10-23 10:28:11 +0200

    TMP s76 workaround in spike_main

--- spike_main/spike.cc
+++ spike_main/spike.cc
@@ -462,6 +462,11 @@ int main(int argc, char** argv)
   if (!*argv1)
     help();
 
+  // mslijepc workaround
+  cfg.set_s76_isa();
+  cfg.set_s76_mem_layout();
+  // end workaround
+
   std::vector<std::pair<reg_t, abstract_mem_t*>> mems =
       make_mems(cfg.mem_layout);
 
