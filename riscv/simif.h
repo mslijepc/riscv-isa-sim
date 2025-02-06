df09501e07c325a37160fb17600bdcf76099d3e8
mslijepc <mladen.slijepcevic@sifive.com>
2025-02-06 13:38:20 +0100

    changed addr_to_mem param

--- riscv/simif.h
+++ riscv/simif.h
@@ -15,7 +15,7 @@ class simif_t
 {
 public:
   // should return NULL for MMIO addresses
-  virtual char* addr_to_mem(reg_t paddr) = 0;
+  virtual char* addr_to_mem(reg_t paddr, const uint8_t* bytes = nullptr) = 0;
   virtual bool reservable(reg_t paddr) { return addr_to_mem(paddr); }
   // used for MMIO addresses
   virtual bool mmio_fetch(reg_t paddr, size_t len, uint8_t* bytes) { return mmio_load(paddr, len, bytes); }
