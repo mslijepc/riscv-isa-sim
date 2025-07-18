5b17bce4e24cba9b093f8e40db7cd7c8dc5ecd99
mslijepc <mladen.slijepcevic@sifive.com>
2025-07-18 21:14:35 +0200

    adding standalone

---
+++ standalone/xrockettest.cc
@@ -0,0 +1,33 @@
+#include <sys/syscall.h>
+#include "rocc.h"
+#include "mmu.h"
+#include <cstring>
+#include <iostream>
+
+class xrockettest_t : public rocc_t
+{
+ public:
+  const char* name() const override { return "rockettest123"; }
+
+  reg_t custom0(processor_t *p, rocc_insn_t insn, reg_t xs1, reg_t xs2) override
+  {
+    std::cout << "xrockettest custom0 called!" << std::endl;
+    switch (insn.funct)
+    {
+      case 0: // ADD operation
+        return xs1 + xs2;
+      default:
+        illegal_instruction(*p);
+    }
+    return 0;
+  }
+
+  xrockettest_t() {
+    std::cout << "xrockettest_t constructor called!" << std::endl;
+  }
+};
+
+REGISTER_EXTENSION(rockettest, []() { 
+  std::cout << "rockettest factory called!" << std::endl;
+  return new xrockettest_t; 
+})
